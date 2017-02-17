60de9353301c83fab1882b0971b71377a2fa5db2
wangdonghui <wangdonghuiibt@gmail.com>
2017-02-17 16:18:38 +0800

    people can's see edit button unless he is group owner

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,6 +29,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.user = current_user
+    
     if @group.save
       redirect_to groups_path
     else
